4163c9018f8e7f4ab81822cbbc0c6e407db6bc37
Sandji <76880281+SandjiGit@users.noreply.github.com>
2023-02-11 12:46:45 +0100

    Initial Commit

---
+++ src/main/java/fr/sandji/sphone/SPhone.java
@@ -0,0 +1,37 @@
+package fr.sandji.sphone;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+        modid = SPhone.MOD_ID,
+        name = SPhone.MOD_NAME,
+        version = SPhone.VERSION
+)
+public class SPhone {
+
+    public static final String MOD_ID = "sphone";
+    public static final String MOD_NAME = "SPhone";
+    public static final String VERSION = "1.0";
+
+    @Mod.Instance(MOD_ID)
+    public static SPhone INSTANCE;
+
+
+    @Mod.EventHandler
+    public void preinit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void postinit(FMLPostInitializationEvent event) {
+
+    }
+}
