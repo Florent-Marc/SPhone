edd3cab76f606933bad29610abbc8db5f64007b4
Sandji <76880281+SandjiGit@users.noreply.github.com>
2023-02-11 19:54:13 +0100

    Add Settings System

---
+++ src/main/java/fr/sandji/sphone/client/phone/apps/settings/PhoneSettings.java
@@ -0,0 +1,44 @@
+package fr.sandji.sphone.client.phone.apps.settings;
+
+import com.google.gson.Gson;
+
+import java.io.*;
+
+public class PhoneSettings {
+    public int phone_number;
+    public int phone_background;
+    public int phone_ring;
+
+    public PhoneSettings(int phone_number, int phone_background, int phone_ring) {
+        this.phone_number = phone_number;
+        this.phone_background = phone_background;
+        this.phone_ring = phone_ring;
+    }
+
+    public void savePhoneSettings(PhoneSettings data) {
+        Gson gson = new Gson();
+        File file = new File("phone_settings.json");
+        try {
+            FileWriter writer = new FileWriter(file);
+            gson.toJson(data, writer);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public PhoneSettings loadPhoneSettings() {
+        Gson gson = new Gson();
+        File file = new File("phone_settings.json");
+        try {
+            PhoneSettings data = gson.fromJson(new FileReader(file), PhoneSettings.class);
+            PhoneSettings phone_settings = new PhoneSettings(data.phone_number, data.phone_background, data.phone_ring);
+            return data;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
---
+++ src/main/java/fr/sandji/sphone/client/phone/apps/settings/Settings.java
@@ -0,0 +1,9 @@
+package fr.sandji.sphone.client.phone.apps.settings;
+
+public class Settings {
+
+    public void ChangeBackground(int phone_background) {
+
+    }
+
+}
