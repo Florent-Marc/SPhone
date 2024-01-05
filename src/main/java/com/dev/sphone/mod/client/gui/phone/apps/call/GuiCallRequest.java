5e67a871a2bb4979efcccc60083bb39a2ced96bd
Ertinox45 <68199251+Ertinox45@users.noreply.github.com>
2024-01-05 20:03:00 +0100

    Fix decline stop sound & npe

--- src/main/java/com/dev/sphone/mod/client/gui/phone/apps/call/GuiCallRequest.java
+++ src/main/java/com/dev/sphone/mod/client/gui/phone/apps/call/GuiCallRequest.java
@@ -49,7 +49,7 @@ public class GuiCallRequest extends GuiHome {
         GuiPanel ButtonAccept = new GuiPanel();
         ButtonAccept.setCssClass("button_accept");
         ButtonAccept.addClickListener((mouseX, mouseY, mouseButton) -> {
-            SPhone.network.sendToServer(new PacketAcceptRequest(true, number, contactTargetName));
+            SPhone.network.sendToServer(new PacketAcceptRequest(number, contactTargetName));
             mc.displayGuiScreen(new GuiCall(this.getGuiScreen(), contact.getName() + " " + contact.getLastname()).getGuiScreen());
         });
         getBackground().add(ButtonAccept);
@@ -57,8 +57,9 @@ public class GuiCallRequest extends GuiHome {
         GuiPanel ButtonDecline = new GuiPanel();
         ButtonDecline.setCssClass("button_decline");
         ButtonDecline.addClickListener((mouseX, mouseY, mouseButton) -> {
-            SPhone.network.sendToServer(new PacketCallRequest(false, receiver));
-            mc.displayGuiScreen(new GuiCallEnd(this.getGuiScreen(), number).getGuiScreen());
+            SPhone.network.sendToServer(new PacketCallRequest(false, number));
+            mc.displayGuiScreen(new GuiHome().getGuiScreen());
+            mc.getSoundHandler().stop("sphone:ringtone", SoundCategory.MASTER);
         });
         getBackground().add(ButtonDecline);
     }
